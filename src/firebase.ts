0ef9b52c63ede99d43af6a1c8e133cde97e93e37
gokulra2006-del <gokulra2006@gmail.com>
2026-07-28 17:59:48 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,15 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBDnkDGCILPF7A0Hc6MBb9VS4q36LHENV8",
-  authDomain: "alert-system-8033b.firebaseapp.com",
-  projectId: "alert-system-8033b",
-  storageBucket: "alert-system-8033b.firebasestorage.app",
-  messagingSenderId: "761056483768",
-  appId: "1:761056483768:web:41cfe2c2f9b9f8ef53d74d",
-  measurementId: "G-FJKJW188MM"
-};
+//give env here
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
